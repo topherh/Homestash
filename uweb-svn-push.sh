1ce35ac71aa98ce656ae941bbf561bfecdf00a4a
Chris Heiland <cheiland@uw.edu>
2011-07-19 17:38:21 -0700

    Unused script

--- uweb-svn-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-notify-send 'UWeb Push Started'
-svn --quiet --force export https://svn.cac.washington.edu/svn/uwplone/UWMarketing/uwmarketing.xdvtheme/trunk/ /home/cheiland/Documents/svnuweb-tmp/
-rsync -aqz --exclude 'president.html' --exclude '.htgroup' --exclude '.htaccess' -e ssh /home/cheiland/Documents/svnuweb-tmp/ uweb@ovid:public_html/redesign/
-notify-send 'UWeb Push Finished'
